2e4a864a096c98403087d3834857813a23afd771
WooheonHong <quasar103@naver.com>
2021-03-09 16:31:39 +0900

    test

--- setup.py
+++ setup.py
@@ -1,14 +1,26 @@
 from setuptools import find_packages, setup
 
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(
     name="wooheon_master_lib",
     version="0.0.1",
     decription="wooheon master code pip install",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url="https://github.com/WooheonHong/personal_master.git",
     author="Wooheon Hong",
-    author_email="quasar103@naver.com",
+    author_email="quasar103@postech.co.kr",
     zip_safe=False,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
     packages=find_packages(),
     license="MIT",
+    install_requires=requirements,
 )
