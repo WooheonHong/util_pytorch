a6a6f7c9190af2a81bd8f985f88192a197e522fb
WooheonHong <quasar103@naver.com>
2021-03-09 15:23:49 +0900

    test

--- setup.py
+++ setup.py
@@ -1,22 +1,22 @@
-import io 
+import io
 from setuptools import find_packages, setup
 
 # Read in the README for the long description on PyPI
 def long_description():
-    with io.open('README.rst', 'r', encoding='utf-8') as f:
+    with io.open("README.rst", "r", encoding="utf-8") as f:
         readme = f.read()
     return readme
 
+
 setup(
-    name='wooheon_master_lib',
-    version='0.0.1',
-    decription='wooheon master code pip install',
+    name="wooheon_master_lib",
+    version="0.0.1",
+    decription="wooheon master code pip install",
     long_description=long_description(),
-    url='https://github.com/WooheonHong/personal_master.git',
-    author='Wooheon Hong',
-    author_email='quasar103@naver.com',
-    packages=['wooheon_master_lib'],
+    url="https://github.com/WooheonHong/personal_master.git",
+    author="Wooheon Hong",
+    author_email="quasar103@naver.com",
     zip_safe=False,
     packages=find_packages(),
-    license='MIT',
+    license="MIT",
 )
